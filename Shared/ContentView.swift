7af2b8b64b170017f7a82f5ec2c14a6f2b6d07a9
monjur35 <monjur35-1019@diu.edu.bd>
2022-09-09 22:22:43 +0600

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Tamanna  on 9/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding().foregroundColor(<#T##SwiftUI.Color?#>)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewLayout(.device)
+    }
+}
---
+++ Shared/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Shared
+//
+//  Created by Tamanna  on 9/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
